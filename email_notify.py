9cdd90e62462d2b77aaf141d5827ac1e54701f3d
Ryan Baltenberger <ryan.baltenberger@gmail.com>
2017-08-17 09:57:37 -0400

    turn this into a function, trim it down

--- email_notify.py
+++ email_notify.py
@@ -1,24 +1,13 @@
-import sys
-
-import argparse 
-parser = argparse.ArgumentParser()
-parser.add_argument("target")
-parser.add_argument("--subject", "-s", default="{}".format(sys.argv[0]))
-parser.add_argument("--message", "-m", default=None)
-args = parser.parse_args()
-
-if args.message is None:
-    args.message = sys.stdin.read()
-
 import smtplib
-from email.mime.text import MIMEText
 import getpass
 
-msg = MIMEText(args.message)
-msg["Subject"] = args.subject
-msg["To"] = args.target
-msg["From"] = "{} ({})".format(sys.argv[0], getpass.getuser())
+def sendEmail(toaddrs, subject, message):
+    # setup the from address
+    fromaddr = 'notification@notify.py'
+    # construct the message
+    msg = 'Subject: {}\r\nFrom: {}\r\nTo: {}\r\n\r\n{}'.format(subject, fromaddr, ', '.join(toaddrs), message)
 
-s = smtplib.SMTP("localhost")
-s.send_message(msg)
-s.quit()
+    # send the message
+    s = smtplib.SMTP('localhost')
+    s.sendmail(fromaddr, toaddrs, msg)
+    s.quit()
--- readme.md
+++
@@ -1,8 +0,0 @@
-# email\_notify.py
-
-A simple email notifier, written in python. Useful for email notifications at the end of long
-running processes.
-
-## example usage
-
-`echo "Hello email!" | python3 email_notify.py <your_email>`
