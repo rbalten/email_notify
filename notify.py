9a7ae270dfff0692b04997ed3133aba324c725d1
Ryan Baltenberger <ryan.baltenberger@gmail.com>
2017-11-07 16:53:36 -0500

    minor text change

--- notify.py
+++ notify.py
@@ -8,7 +8,7 @@ def sendEmail(toaddrs, subject, message):
     msg = 'Subject: {}\r\nFrom: {}\r\nTo: {}\r\n\r\n{}'.format(subject, fromaddr, ', '.join(toaddrs), message)
 
     # send the message
-    # NOTE: this requires a local SMTP server to be running
+    # NOTE (ryan): this requires a local SMTP server to be running
     s = smtplib.SMTP('localhost')
     s.sendmail(fromaddr, toaddrs, msg)
     s.quit()
